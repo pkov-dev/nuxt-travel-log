dbc9cdec9f5167606586ed1b67e054ecf3c70997
Pavlo Kovalenko <pashko.dev@gmail.com>
2026-08-05 09:46:46 +0300

    :sparkles: setup eslint / lint-staged

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import antfu from "@antfu/eslint-config";
+
+// @ts-check
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
+export default withNuxt(antfu({
+  type: "app",
+  typescript: true,
+  formatters: true,
+
+  stylistic: {
+    indent: 2,
+    semi: true,
+    quotes: "double",
+  },
+  ignores: ["./src/github/schema.docs.graphql"],
+}, {
+  rules: {
+    "ts/consistent-type-definitions": ["error", "type"],
+    "no-console": ["off"],
+    "antfu/no-top-level-await": ["off"],
+    "node/prefer-global/process": ["off"],
+    "perfectionist/sort-imports": ["error", {
+
+      tsconfig: {
+        rootDir: ".",
+      },
+    }],
+    "unicorn/filename-case": ["error", {
+      cases: {
+        camelCase: true,
+        kebabCase: true,
+      },
+      ignore: ["README.md"],
+    }],
+  },
+}),
+);
