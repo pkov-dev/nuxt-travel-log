92ad9e519e9cb593c507e3e450926ef4d0075732
Pavlo Kovalenko <pashko.dev@gmail.com>
2026-08-06 10:30:36 +0300

    :sparkles: setup tailwind / daisy ui / icons (#49)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,19 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  modules: ["@nuxt/eslint", "@nuxt/icon"],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
   eslint: {
     config: {
       standalone: false,
     },
   },
+  css: ["~/assets/css/main.css"],
 });
