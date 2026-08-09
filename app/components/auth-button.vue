66c55b712d3eb0618620d2da339ebf7a96f42924
Pavlo Kovalenko <pashko.dev@gmail.com>
2026-08-09 19:16:57 +0300

    :sparkles: add github oauth (#55)

---
+++ app/components/auth-button.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const authStore = useAuthStore();
+</script>
+
+<template>
+  <button :disabled="authStore.loading" class="btn btn-accent" @click="authStore.signIn">
+    Sing In With Github
+    <span v-if="authStore.loading" class="loading loading-spinner loading-md" />
+    <Icon v-else name="tabler:brand-github" size="24" />
+  </button>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,10 +8,7 @@
         <p class="py-6">
           Keep track of your travels and adventures with this simple travel log
         </p>
-        <button class="btn btn-accent">
-          Sing In With Github
-          <Icon name="tabler:brand-github" size="24" />
-        </button>
+        <AuthButton />
       </div>
     </div>
   </div>
