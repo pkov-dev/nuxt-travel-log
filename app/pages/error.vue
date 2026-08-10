2a4495b5b4fbcffb7dd4a7ea59c4b98c19ba0120
Pavlo Kovalenko <pashko.dev@gmail.com>
2026-08-10 12:52:18 +0300

    :sparkles: protect dashboard route (#57)

--- app/pages/error.vue
+++ app/pages/error.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 const route = useRoute();
-const error = route.query.error || "An unknown error occured.";
+const error = route.query.error || "An unknown error occurred.";
 </script>
 
 <template>
