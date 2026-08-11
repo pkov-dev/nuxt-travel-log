afcfc172412da3c19779ec4197a76bef53d4077f
Pavlo Kovalenko <pashko.dev@gmail.com>
2026-08-11 11:41:18 +0300

    :bug: fix auth session refetch

--- app/stores/auth.ts
+++ app/stores/auth.ts
@@ -6,7 +6,13 @@ export const useAuthStore = defineStore("useAuthStore", () => {
   const session = ref<Awaited<ReturnType<typeof authClient.useSession>> | null>(null);
 
   async function init() {
-    const data = await authClient.useSession(useFetch);
+    const data = await authClient.useSession((url, options) => {
+      return useFetch(url, {
+        ...options,
+        key: "better-auth-session",
+      });
+    });
+
     session.value = data;
   }
 
