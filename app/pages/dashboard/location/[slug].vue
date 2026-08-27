239bdad6ad1e6e8b634ad4258dc36be74b4c922d
Pavlo Kovalenko <pashko.dev@gmail.com>
2026-08-27 13:57:47 +0300

    :sparkles: add location deletion (#76)

--- app/pages/dashboard/location/[slug].vue
+++ app/pages/dashboard/location/[slug].vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import type { FetchError } from "ofetch";
+
 const route = useRoute();
 const mapStore = useMapStore();
 
@@ -7,8 +9,15 @@ const {
   status,
   error,
   refresh,
-} = useCurrentLocation(
-);
+} = useCurrentLocation();
+
+const isOpen = ref(false);
+const isDeleting = ref(false);
+const deleteError = ref("");
+
+const loading = computed(() => status.value === "pending" || isDeleting.value);
+const errorMessage = computed(() => error.value?.statusText || deleteError.value);
+
 watch(location, (newLocation) => {
   if (newLocation) {
     mapStore.detailedPoint = newLocation;
@@ -16,6 +25,31 @@ watch(location, (newLocation) => {
 }, {
   immediate: true,
 });
+
+function openDialog() {
+  isOpen.value = true;
+  (document.activeElement as HTMLAnchorElement).blur();
+}
+
+async function confirmDelete() {
+  try {
+    isOpen.value = false;
+    deleteError.value = "";
+    isDeleting.value = true;
+
+    await $fetch(`/api/locations/${route.params.slug}`, {
+      method: "DELETE",
+    });
+
+    navigateTo("/dashboard");
+  }
+  catch (e) {
+    const error = e as FetchError;
+
+    deleteError.value = getFetchErrorMessage(error);
+  }
+  isDeleting.value = false;
+}
 
 onBeforeUnmount(() => {
   mapStore.detailedPoint = null;
@@ -30,18 +64,42 @@ onBeforeRouteUpdate((to) => {
 
 <template>
   <div class="p-4 min-h-64">
-    <div v-if="status === 'pending'">
+    <div v-if="loading">
       <div class="loading" />
     </div>
 
-    <div v-if="error && status !== 'pending'" class="alert alert-error">
+    <div v-if="errorMessage && !loading" class="alert alert-error">
       <h2 class="text-lg ">
-        {{ error.statusText }}
+        {{ errorMessage }}
       </h2>
     </div>
-    <div v-if="route.name === 'dashboard-location-slug' && location && status !== 'pending'">
+    <div v-if="route.name === 'dashboard-location-slug' && location && !loading">
       <h2 class="text-xl">
         {{ location.name }}
+
+        <div class="dropdown dropdown-bottom">
+          <div
+            tabindex="0"
+            role="button"
+            class="btn m-0 btn-sm p-0 border-transparent"
+          >
+            <Icon name="tabler:dots-vertical" size="20" />
+          </div>
+          <ul tabindex="-1" class="dropdown-content menu bg-base-100 rounded-box z-1 p-2 shadow-sm">
+            <li>
+              <NuxtLink @click="openDialog">
+                <Icon name="tabler:trash-x-filled" size="20" />
+                Delete
+              </NuxtLink>
+            </li>
+            <li>
+              <NuxtLink :to="{ name: 'dashboard-location-slug-edit', params: { slug: route.params.slug } }">
+                <Icon name="tabler:map-pin-cog" size="20" />
+                Edit
+              </NuxtLink>
+            </li>
+          </ul>
+        </div>
       </h2>
       <p class="text-sm">
         {{ location.description }}
@@ -59,5 +117,14 @@ onBeforeRouteUpdate((to) => {
     <div v-if="route.name !== 'dashboard-location-slug'">
       <NuxtPage />
     </div>
+    <AppDialog
+      :is-open
+      title="Are you sure"
+      description="Deleting this location will also delete all of the associated logs. This cannot be undone. Do you really want to do this"
+      confirm-label="Yes, I delete this location"
+      confirm-class="btn-error"
+      @on-closed="isOpen = false"
+      @on-confirmed="confirmDelete"
+    />
   </div>
 </template>
--- lib/db/queries/location.ts
+++ lib/db/queries/location.ts
@@ -75,3 +75,12 @@ export async function updateLocationBySlug(updates: InsertLocation, slug: string
 
   return updated;
 }
+
+export async function removeLocationBySlug(slug: string, userId: number) {
+  const [removed] = await db.delete(location).where(and(
+    eq(location.slug, slug),
+    eq(location.userId, userId),
+  )).returning();
+
+  return removed;
+}
